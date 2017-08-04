295fdffab8823f7f071ffb84a734f57bb5f3d3b5
King-Nivek <King-Nivek@users.noreply.github.com>
2017-08-04 16:18:26 -0400

    Add two DefaultController Actions and their Routes
    --  Adds a route to the Home (index) page Action
    --  Adds a route to the About page Action

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,4 +8,29 @@ use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/", name="home")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'list' => array(
+                array('address' => $this->generateUrl('home'), 'name' => 'Home')
+            )
+        ]);
+    }
+
+    /**
+    * @Route("/about", name="about")
+    */
+    public function aboutAction(Request $request)
+    {
+        return $this->render('fragments/_about.html.twig', [
+            'list' => array(
+                array('address' => $this->generateUrl('home'), 'name' => 'Home'),
+                array('address' => $this->generateUrl('about'), 'name' => 'About')
+            )
+        ]);
+    }
 }
